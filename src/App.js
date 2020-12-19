01282d9bdf50aaf8c1236f43ba200ec380f97ed8
lauradoc <laurahdocherty@gmail.com>
2020-12-18 21:48:03 -0600

    Front End Masters - Intro to React

---
+++ src/App.js
@@ -0,0 +1,31 @@
+const Pet = ({ name, animal, breed }) => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, name),
+    React.createElement("h2", {}, animal),
+    React.createElement("h2", {}, breed)
+  ]);
+};
+
+
+const App = ()=> {
+  return React.createElement("div", { id: "something-important"}, [
+    React.createElement("h1", {}, "Adopt Me!"),
+    React.createElement(Pet, {
+      name: "Grotto", 
+      animal: "dog", 
+      breed: "German Sherpherd"
+    }),
+    React.createElement(Pet, {
+      name: "Speedy", 
+      animal: "frog", 
+      breed: "Froggy Frog"
+    }),
+    React.createElement(Pet, {
+      name: "Rocky", 
+      animal: "dog", 
+      breed: "Havanese"
+    })
+  ]);
+};
+
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
