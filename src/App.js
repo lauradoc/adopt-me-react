67fbe7a2af3464c6c83b5d89913c698864876d85
lauradoc <laurahdocherty@gmail.com>
2020-12-19 17:11:50 -0600

    babel build error --- need to resolve

--- src/App.js
+++ src/App.js
@@ -3,24 +3,33 @@ import { render } from "react-dom";
 import Pet from "./Pet";
 
 const App = () => {
-  return React.createElement("div", { id: "something-important" }, [
-    React.createElement("h1", {}, "Adopt Me!"),
-    React.createElement(Pet, {
-      name: "Grotto",
-      animal: "dog",
-      breed: "German Sherpherd",
-    }),
-    React.createElement(Pet, {
-      name: "Speedy",
-      animal: "frog",
-      breed: "Froggy Frog",
-    }),
-    React.createElement(Pet, {
-      name: "Rocky",
-      animal: "dog",
-      breed: "Havanese",
-    }),
-  ]);
+  // return React.createElement("div", { id: "something-important" }, [
+  //   React.createElement("h1", {}, "Adopt Me!"),
+  //   React.createElement(Pet, {
+  //     name: "Grotto",
+  //     animal: "dog",
+  //     breed: "German Sherpherd",
+  //   }),
+  //   React.createElement(Pet, {
+  //     name: "Speedy",
+  //     animal: "frog",
+  //     breed: "Froggy Frog",
+  //   }),
+  //   React.createElement(Pet, {
+  //     name: "Rocky",
+  //     animal: "dog",
+  //     breed: "Havanese",
+  //   }),
+  // ]);
+
+  return (
+    <div>
+      <h1 id="something-important">Adopt Me!</h1>
+      <Pet name="Gotto" animal="dog" breed="German Shepherd" />
+      <Pet name="Speedy" animal="frog" breed="water frog" />
+      <Pet name="Rocky" animal="dog" breed="Havanese" />
+    </div>
+  )
 };
 
-render(React.createElement(App), document.getElementById("root"));
+render(<App />, document.getElementById("root"));
