164dc73f6b92f88da7b70a1df5b4c886021fe4a7
lauradoc <laurahdocherty@gmail.com>
2020-12-19 15:16:47 -0600

    add npm scripts, eslint, prettier

--- src/App.js
+++ src/App.js
@@ -2,29 +2,28 @@ const Pet = ({ name, animal, breed }) => {
   return React.createElement("div", {}, [
     React.createElement("h1", {}, name),
     React.createElement("h2", {}, animal),
-    React.createElement("h2", {}, breed)
+    React.createElement("h2", {}, breed),
   ]);
 };
 
-
-const App = ()=> {
-  return React.createElement("div", { id: "something-important"}, [
+const App = () => {
+  return React.createElement("div", { id: "something-important" }, [
     React.createElement("h1", {}, "Adopt Me!"),
     React.createElement(Pet, {
-      name: "Grotto", 
-      animal: "dog", 
-      breed: "German Sherpherd"
+      name: "Grotto",
+      animal: "dog",
+      breed: "German Sherpherd",
     }),
     React.createElement(Pet, {
-      name: "Speedy", 
-      animal: "frog", 
-      breed: "Froggy Frog"
+      name: "Speedy",
+      animal: "frog",
+      breed: "Froggy Frog",
     }),
     React.createElement(Pet, {
-      name: "Rocky", 
-      animal: "dog", 
-      breed: "Havanese"
-    })
+      name: "Rocky",
+      animal: "dog",
+      breed: "Havanese",
+    }),
   ]);
 };
 
